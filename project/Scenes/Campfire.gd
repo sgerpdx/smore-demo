b64a2504d4cb07dbe69ceb343433763e91898ac5
SamuelHGerber <henrylightfoot@gmail.com>
2021-09-04 13:27:26 -0700

    burn stat tracking and feedback

--- project/Scenes/Campfire.gd
+++ project/Scenes/Campfire.gd
@@ -1,9 +1,32 @@
 extends Area2D
 
-
+onready var player = get_parent().get_node("player/Player/Arm/Sprite2/Marshmallow_K2D/camping-marshmallow")
+var burning = false
+var doneness = 0
+var burnValue = 0
+var burnThreshold = 10
 
 
 func _on_Campfire_body_entered(body):
-	var burnThing = body.get_name()
+	var burnThing = body.is_on_ceiling()
+	burning = true
+		
 	print("burn")
 	print(burnThing)
+	print(burning)
+	
+func _on_Campfire_body_exited(body):
+	burning = false
+	burnValue = 0
+
+func _process(delta):
+	if burning:
+		doneness += 0.25 * delta
+		burnValue += 5 * delta
+		print("Done:", doneness)
+		print("Burn:", burnValue)
+	if burnValue >= burnThreshold:
+		print("Your marshmallow is on fire!")
+
+
+
